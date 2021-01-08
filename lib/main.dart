1b355e012c73e6a9cb5622a4a41986491833ca82
macOS <macos@macOSs-iMac.local>
2021-01-08 10:25:11 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:timer/timer/bloc/timer_bloc.dart';
+import 'package:timer/timer/ticker.dart';
+import 'package:timer/timer/timer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color.fromRGBO(109, 234, 255, 1),
+        accentColor: Color.fromRGBO(72, 74, 126, 1),
+        brightness: Brightness.dark,
+      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Timer',
+      home: BlocProvider(
+        create: (context) => TimerBloc(ticker: Ticker()),
+        child: Timer(),
+      ),
+    );
+  }
+}
